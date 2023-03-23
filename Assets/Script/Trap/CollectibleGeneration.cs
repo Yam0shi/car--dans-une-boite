2c789ccd6383c08dbe999c750851a2ccba321242
Yam0shi <tessiera00@gmail.com>
2023-03-23 10:47:55 +0100

    collectible add

---
+++ Assets/Script/Trap/CollectibleGeneration.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEditor.Progress;
+
+public class CollectibleGeneration : MonoBehaviour
+{
+    [SerializeField] Collectible myCollectible;
+    [SerializeField] bool spawnOrNot;
+    [SerializeField] int YoN;
+    void Start()
+    {
+        YoN = (int)Random.Range(0, 2);
+        if(YoN == 0)
+        {
+            spawnOrNot = false;
+        }
+        else
+        {
+            spawnOrNot = true;
+        }
+
+        if(spawnOrNot )
+        {
+            LoadItem(myCollectible);
+        }
+    }
+    void LoadItem(Collectible data)
+    {
+        foreach (Transform child in transform)
+        {
+            if (Application.isEditor) DestroyImmediate(child.gameObject);
+            else Destroy(child.gameObject);
+        }
+
+        GameObject visuals = Instantiate(myCollectible.apparence);
+        visuals.transform.SetParent(transform);
+        visuals.transform.localPosition = Vector3.zero;
+        visuals.transform.rotation = Quaternion.identity;
+        visuals.tag = "Item";
+        visuals.layer = LayerMask.NameToLayer("Items");
+    }
+}
