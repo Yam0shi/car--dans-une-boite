268d7a404fcabbab113e69f5d6af24571e401c1f
Yam0shi <tessiera00@gmail.com>
2023-04-17 18:31:20 +0200

    shop patch save de ce qui a été acheté

--- Assets/Script/General/SaveSkin.cs
+++ Assets/Script/General/SaveSkin.cs
@@ -1,11 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class SaveSkin : MonoBehaviour
 {
     public Sprite[] SkinBought;
-    void Update()
+
+    private void Update()
     {
         DontDestroyOnLoad(gameObject);
 
--- Assets/Script/General/Skin.cs
+++ Assets/Script/General/Skin.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,9 +5,28 @@ public class Skin : MonoBehaviour
 {
     [SerializeField] GameObject saveSkinbuy;
     public bool buyBool;
+    public float etatDachat;
+    public int buttonIndex;
+
+    public void Start()
+    {
+        etatDachat = PlayerPrefs.GetInt("étatDuBooléen"+buttonIndex);
+
+        if(etatDachat == 1)
+        {
+            buyBool = true;
+        }
+    }
+    public void Update()
+    {
+        if (buyBool)
+        {
+            gameObject.transform.GetChild(0).gameObject.SetActive(false);
+            gameObject.transform.GetChild(1).gameObject.SetActive(true);
+        }
+    }
     public void BuySkin(int needValue)
     {
-
         if (!buyBool)
         {
             if (Points.GoldOfKinzo >= needValue)
@@ -22,10 +39,12 @@ public class Skin : MonoBehaviour
         }
     }
 
-    public void SetIndex(int buttonIndex)
+    public void SetIndex()
     {
         if(buyBool == true)
         {
+            PlayerPrefs.SetInt("étatDuBooléen" + buttonIndex, 1);
+            Debug.Log(PlayerPrefs.GetInt("étatDuBooléen" + buttonIndex));
             gameObject.transform.GetChild(0).gameObject.SetActive(false);
             saveSkinbuy.GetComponent<SaveSkin>().SkinBought[buttonIndex] = gameObject.GetComponent<Image>().sprite;
             gameObject.transform.GetChild(1).gameObject.SetActive(true);
@@ -34,6 +53,6 @@ public class Skin : MonoBehaviour
 
     public void UseSkin()
     {
-      gameObject.transform.GetChild(1).gameObject.SetActive(false);
+        gameObject.transform.GetChild(1).gameObject.SetActive(false);
     }
 }
